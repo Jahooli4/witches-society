eccf8b1b8316e05b226a6d095c4fa06d7239faa5
Michelle Jones <michelle.jones.2161@hotmail.co.uk>
2025-01-09 15:17:49 +0000

    chore: connect to postgres and create superuser

--- booking/views.py
+++ booking/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<h1>Booking home</h1>')
+    return render(request, 'booking/home.html')
 
 def about(request):
-    return HttpResponse('<h1>Booking about</h1>')
+    return render(request, 'booking/about.html')
