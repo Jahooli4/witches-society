c8a891a8fa193757005413852a5f04e726b2b7df
Michelle Jones <michelle.jones.2161@hotmail.co.uk>
2025-01-10 11:46:00 +0000

    fix: add secret key to env.py and edit url paths for booking form

--- booking/urls.py
+++ booking/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='booking-home'),
-    path('about/', views.about, name='booking-about'),
+    path('booking_form/', views.booking_form, name='booking-form'),
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from datetime import datetime, timedelta
+from .models import *
+from django.contrib import messages
 
 # Create your views here.
 
 def home(request):
     return render(request, 'booking/home.html')
 
-def about(request):
-    return render(request, 'booking/about.html')
+def booking_form(request):
+    return render(request, 'booking/booking_form.html')
