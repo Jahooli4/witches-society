3c9082523847e87ce155ac7a0db2cc76708174b3
Michelle Jones <michelle.jones.2161@hotmail.co.uk>
2025-01-11 13:22:11 +0000

    feat: create about.html file and connect up the urls

--- website/urls.py
+++ website/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='website-home'),
     path('blog/', views.blog, name='website-blog'),
+    path('about/', views.about, name='website-about'),
 ]
--- website/views.py
+++ website/views.py
@@ -23,6 +23,9 @@ posts = [
 def home(request):
     return render(request, 'website/home.html', {'title': 'Home'})
 
+def about(request):
+    return render(request, 'website/about.html', {'title': 'About'})
+
 def blog(request):
     context = {
         'posts': posts
