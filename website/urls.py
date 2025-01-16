2fb876c2a42a733cf4f992da5528efb7b572e193
Michelle Jones <michelle.jones.2161@hotmail.co.uk>
2025-01-16 18:14:10 +0000

    feat: link up a new page that shows posts by a certain user when their name is clicked

--- website/urls.py
+++ website/urls.py
@@ -4,12 +4,14 @@ from .views import (
     PostDetailView, 
     PostCreateView, 
     PostUpdateView, 
-    PostDeleteView
+    PostDeleteView,
+    UserPostListView
     )
 from . import views
 
 urlpatterns = [
     path('', views.home, name='website-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('blog/', PostListView.as_view(), name='website-blog'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
